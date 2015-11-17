cde938248ae2c5153976c072edcf5f06b0f0256c
ankit1200 <ankit1200@gmail.com>
2015-11-16 19:32:49 -0500

    clean up

--- 1channel/CollectionViewController.swift
+++ 1channel/CollectionViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UISearchBarDelegate {
+class CollectionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
 
     var filteredSeriesList = Array<Episode>()
     var seriesList = Array<Episode>()
@@ -22,6 +22,23 @@ class CollectionViewController: UIViewController, UICollectionViewDataSource, UI
     @IBOutlet var movieRefresh: UIBarButtonItem!
     @IBOutlet var searchBar: UISearchBar!
     @IBOutlet var dismissKeyboardButton: UIButton!
+    
+    
+    // Structs to check iphone screen size
+    struct ScreenSize
+    {
+        static let SCREEN_WIDTH = UIScreen.mainScreen().bounds.size.width
+        static let SCREEN_HEIGHT = UIScreen.mainScreen().bounds.size.height
+        static let SCREEN_MAX_LENGTH = max(ScreenSize.SCREEN_WIDTH, ScreenSize.SCREEN_HEIGHT)
+        static let SCREEN_MIN_LENGTH = min(ScreenSize.SCREEN_WIDTH, ScreenSize.SCREEN_HEIGHT)
+    }
+    
+    struct DeviceType
+    {
+        static let IS_IPHONE_5 = UIDevice.currentDevice().userInterfaceIdiom == .Phone && ScreenSize.SCREEN_MAX_LENGTH == 568.0
+        static let IS_IPHONE_6 = UIDevice.currentDevice().userInterfaceIdiom == .Phone && ScreenSize.SCREEN_MAX_LENGTH == 667.0
+        static let IS_IPHONE_6P = UIDevice.currentDevice().userInterfaceIdiom == .Phone && ScreenSize.SCREEN_MAX_LENGTH == 736.0
+    }
     
     override func viewDidLoad() {
         // Populate Arrays
@@ -100,6 +117,16 @@ class CollectionViewController: UIViewController, UICollectionViewDataSource, UI
         selectedIndex = indexPath.row
     }
     
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+        
+        if (DeviceType.IS_IPHONE_5) {
+            print("test")
+            return UIEdgeInsetsMake(25, 9, 0, 9)
+        } else {
+            print("test234567890")
+            return UIEdgeInsetsMake(25, 20, 0, 20)
+        }
+    }
     
     // MARK: Segment Value Changed
     @IBAction func segmentValueChanged(sender: UISegmentedControl) {
